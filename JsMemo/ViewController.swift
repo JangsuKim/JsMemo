91484bf90cc192811e93a8283166bd7468ae285a
JangsuKim <jangsoo719@gmail.com>
2022-03-12 23:05:06 +0900

    Initial Commit

---
+++ JsMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsMemo
+//
+//  Created by Jangsu Kim on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
